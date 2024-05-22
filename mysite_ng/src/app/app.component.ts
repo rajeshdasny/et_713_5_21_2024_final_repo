b0623dff6e2ba64359dfef6a773f5da2d7b5a6e5
EC2 Default User <ec2-user@ip-172-31-13-93.ec2.internal>
2024-05-22 23:26:56 +0000

    Live Website

--- mysite_ng/src/app/app.component.ts
+++ mysite_ng/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './header/header.component'
+import { JumbotronComponent } from './jumbotron/jumbotron.component'
+import { ContentComponent } from './content/content.component'
+import { FooterComponent } from './footer/footer.component'
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, JumbotronComponent, ContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
